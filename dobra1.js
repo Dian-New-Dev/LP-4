c1b1933b57f577b8bfdd629b7d14f77b38d0cb86
Diego <diannewdev@gmail.com>
2024-04-17 14:35:07 -0400

    mobile ready, on to desktop resolution

--- dobra1.js
+++ dobra1.js
@@ -6,6 +6,13 @@ const h2A = document.querySelector('.h2A');
 const h1B = document.querySelector('.h1B');
 const h2B = document.querySelector('.h2B');
 
+window.onload = function() {
+    trocarDobra1();
+  };
+  
+
+setInterval(trocarDobra1, 10000);
+
 setaEsquerda.addEventListener('click', function() {
     setaEsquerda.style.transform = 'scale(1.2)';
 
@@ -31,17 +38,12 @@ function trocarDobra1() {
     i++
     if (i % 2 === 0) {
         dobra1.style.backgroundImage = "url('images/dobra1-A.webp')";
-        h1A.style.display = 'block';
-        h1A.style.opacity = '1';
-        h2A.style.display = 'block';
-        h1B.style.display = 'none';
-        h2B.style.display = 'none';
+        h1A.innerText = 'JUSTIÇA COM SERIEDADE E HUMANIDADE';
+        h2A.innerText = 'Servindo Campo Grande a Mais de 15 Anos'
     } else {
         dobra1.style.backgroundImage = "url('images/dobra1-B.webp')";
-        h1A.style.display = 'none';
-        h2A.style.display = 'none';
-        h1B.style.display = 'block';
-        h2B.style.display = 'block';
+        h1A.innerText = 'ADVOCACIA PROFISSIONAL E PRECISA';
+        h2A.innerText = 'Serviço eficiente, trato amigável'
 
     }
     
